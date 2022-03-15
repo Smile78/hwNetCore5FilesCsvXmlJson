8d9284b0e2140bfcdd7bf60df5df0025b530d10c
Smile78 <dronussr@gmail.com>
2022-03-15 14:33:24 +0300

    1csvToJson 2xmlToJson

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,11 +5,19 @@ import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
@@ -26,9 +34,7 @@ public class Main {
             clmn.setColumnMapping(columnMapping);
 
             CsvToBean<Employee> csv = new CsvToBeanBuilder<Employee>(empl).withMappingStrategy(clmn).build();
-
             list = csv.parse();
-
             empl.close();
 
         } catch (IOException e) {
@@ -38,59 +44,102 @@ public class Main {
     }
 
     private static <T> String listToJson(List<Employee> list) {
-        Type listType = new TypeToken<List<T>>() {}.getType();
-
+        Type listType = new TypeToken<List<T>>() { }.getType();
         GsonBuilder builder = new GsonBuilder();
         Gson gson = builder.create();
-//        System.out.println(gson.toJson(list.get(0)));
-
         return gson.toJson(list, listType);
     }
 
+    private static void writeString(String json, int nmbr) {
 
-    private static void writeString(String json) {
-
-        try (FileWriter file = new FileWriter("data.json")) {
-//            file.write(obj.toJSONString());
+        try (FileWriter file = new FileWriter("data1"+Integer.toString(nmbr)+".json")) {
             file.write(json);
             file.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
+    public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException {
 
-    public static void main(String[] args)   {
-
-
-        //*******
-        //1ое задание   CSV в JSON
+                         // *******
+                         // 1ое задание   CSV в JSON
 
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
         String fileName = "data.csv";
 
         List<Employee> list = parseCSV(columnMapping, fileName);
-//       System.out.println(list.get(0).firstName);
 
-        //для наглядности...
+                    //для наглядности...
         for (Employee empl : list) {
             empl.firstName = empl.firstName.replaceAll("CSV", "JSON");
         }
 
-        String json = listToJson(list);   // типо туСтринга в строку для созадния CSV
-//       System.out.println(json);
+        String json = listToJson(list);
 
-        writeString(json);
+        writeString(json,1);
 
 
+
+                     // *******
+                     // 2ое задание   XML в JSON
+
+        List<Employee> list2Empl = parseXML("data.xml");
+
+                  //для наглядности...
+        for (Employee empl : list2Empl) {
+            empl.firstName = empl.firstName.replaceAll("XML", "JSON2");
+        }
+
+        String json2 = listToJson(list2Empl);
+        writeString(json2,2);
 
     }
 
+    private static List<Employee> parseXML(String s) throws ParserConfigurationException, IOException, SAXException {
 
+        List<Employee> list1 = new ArrayList<>();
 
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
 
+//        Document doc = builder.parse(new File("company.xml"));
+        Document doc = builder.parse(new File("data.xml"));
 
+        Node root = doc.getDocumentElement(); //получили корневой узел
+        System.out.println("\n" + "Корневой элемент: " + root.getNodeName() + "\n");
 
+        NodeList nodeList = root.getChildNodes();                                // получ список узлов из корневого
 
+        for (int i = 0; i < nodeList.getLength(); i++) {                         // прошли по спсику узлов
+            Node node1 = nodeList.item(i);                                       // узел
+
+            if (Node.ELEMENT_NODE == node1.getNodeType()) {                      // и внтури в конце рекурсия
+                System.out.println("Текущий узел: " + node1.getNodeName());      // ok
+
+                NodeList nodeList2 = node1.getChildNodes();
+
+                String[] mapEmp = new String[5];
+                int cnt =0;
+
+                for (int j = 0; j < nodeList2.getLength(); j++) {                                // прошли по спсику узлов
+                    Node node2 = nodeList2.item(j);
+
+                    if (Node.ELEMENT_NODE == node2.getNodeType()) {                             // и внтури в конце рекурсия
+//                        System.out.println("Текущий узел21: " + node2.getNodeName());         // ok
+//                        System.out.println("Текущий узел22: " + node2.getTextContent());      // ok
+                        Element element = (Element) node2;                                      // элемент
+                        String map = element.getTextContent() ;
+//                        System.out.println(map);
+                        mapEmp[cnt++]=map;
+                    }
+                }
+//                System.out.println(Arrays.toString(mapEmp));
+                list1.add(new Employee(Integer.parseInt(mapEmp[0]), mapEmp[1], mapEmp[2], mapEmp[3], Integer.parseInt(mapEmp[4])));
+//                System.out.println("ls "+list1.size());
+            }
+        }
+        return list1;
+    }
 }
+
